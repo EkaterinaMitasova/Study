b576571d692e645c93006f4fc391e343f595f641
Ekaterina Mitasova <mitasova@i-teco.ru>
2020-09-19 17:54:38 +0300

    Площадь круга

---
+++ src/main/lesson1/task1/Square.java
@@ -0,0 +1,27 @@
+package main.lesson1.task1;
+
+public class Square {
+    int radius = 0;
+    int radius1 = 2;
+    int radius2 = 100;
+
+    public static void main(String[] args) {
+        Square instance = new Square();
+        // System.out.println(instance.getSquare());
+        instance.printSquare();
+    }
+
+    double getSquare() {
+        return 3.14 * radius * radius;
+    }
+
+    void printSquare() {
+        double square = 3.14 * radius * radius;
+        double square1 = 3.14 * radius1 * radius1;
+        double square2 = 3.14 * radius2 * radius2;
+        System.out.println(square);
+        System.out.println(square1);
+        System.out.println(square2);
+
+    }
+}
